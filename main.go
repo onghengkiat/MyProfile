9582702d9e8f63889aa1743b52a7a0d893c43778
Ong Heng Kiat <onghengkiat105@gmail.com>
2021-09-26 20:50:17 +0800

    First commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+
+func main() {
+
+	// Make HTTP GET request
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/achievement", achievementPage)
+	http.HandleFunc("/experience", experiencePage)
+	http.HandleFunc("/project", projectPage)
+
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.ListenAndServe(":8088", nil)
+}
+
+func homePage(writer http.ResponseWriter, request *http.Request) {
+	files := []string{
+		"static/html/MyPage.html",
+		"static/html/base_template.html",
+	}
+
+	template, _ := template.ParseFiles(files[0], files[1])
+	template.Execute(writer, nil)
+}
+
+func achievementPage(writer http.ResponseWriter, request *http.Request) {
+	files := []string{
+		"static/html/AchievementPage.html",
+		"static/html/base_template.html",
+	}
+
+	template, _ := template.ParseFiles(files[0], files[1])
+	template.Execute(writer, nil)
+}
+
+func experiencePage(writer http.ResponseWriter, request *http.Request) {
+	files := []string{
+		"static/html/ExperiencePage.html",
+		"static/html/base_template.html",
+	}
+
+	template, _ := template.ParseFiles(files[0], files[1])
+	template.Execute(writer, nil)
+}
+
+func projectPage(writer http.ResponseWriter, request *http.Request) {
+	files := []string{
+		"static/html/ProjectPage.html",
+		"static/html/base_template.html",
+	}
+
+	template, _ := template.ParseFiles(files[0], files[1])
+	template.Execute(writer, nil)
+}
